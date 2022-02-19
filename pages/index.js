b976fff7337143c569fafa57d844ded5c8f72bcf
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-02-19 23:23:24 +0300

    getServerSideProps and getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
-import Link from 'next/link';
 import Layout from "@/components/Layout";
+import {API_URL} from "@/config/index";
 
-export default function Home() {
-  return (
-      <Layout>
-          <h1>Home</h1>
-      </Layout>
-  )
+export default function Home({events}) {
+    return (
+        <Layout>
+            <h1>Upcoming Events</h1>
+        </Layout>
+    )
+}
+
+//getServerSideProps
+export async function getStaticProps() {
+    const res = await fetch(`${API_URL}/api/events`);
+    const events = await res.json();
+
+    // console.log(events);
+
+    return {
+        props: {
+            events
+        },
+        revalidate: 1
+    }
 }
