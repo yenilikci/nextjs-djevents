d9f24bf435ba8b5f42f79366a873b53c4f69af6a
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-03-01 22:03:01 +0300

    login and register forms

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a5267182e4301336"; }
+/******/ 		__webpack_require__.h = function() { return "c54065eeb14341ad"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
