4f37831d544d9b41914ce315fe4134712a924f9b
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-03-27 17:27:51 +0300

    display dashboard events

--- client/pages/account/dashboard.js
+++ client/pages/account/dashboard.js
@@ -1,12 +1,24 @@
 import {parseCookies} from "@/helpers/index";
 import Layout from "@/components/Layout";
 import {API_URL} from "@/config/index";
+import styles from "@/styles/Dashboard.module.css";
+import DashboardEvent from "@/components/DashboardEvent";
 
 export default function DashboardPage({events}) {
+
+    const deleteEvent = (id) => {
+        console.log(id);
+    }
+
     console.log(events)
     return (
         <Layout title="User Dashboard">
-            <h1>Dashboard</h1>
+            <div className={styles.dash}>
+                <h1>Dashboard</h1>
+                <h3>My Events</h3>
+
+                {events.map((evt) => (<DashboardEvent key={evt.id} evt={evt} handleDelete={deleteEvent}/>))}
+            </div>
         </Layout>
     );
 }
